5f9d1d625be9e0b8e9558d75fb7bff46c084f10b
Spri85 <viktorpar@gmail.com>
2017-09-25 15:30:42 +0300

    added binding between component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Recipe } from './../recipe.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,14 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-  new Recipe('A test Recipe', 'Thi is a simple test', 'http://www.seriouseats.com/recipes/assets_c/2016/01/20160116-american-beef-stew-recipe-34-thumb-1500xauto-429351.jpg' ),
-  new Recipe('A test Recipe', 'Thi is a simple test', 'http://www.seriouseats.com/recipes/assets_c/2016/01/20160116-american-beef-stew-recipe-34-thumb-1500xauto-429351.jpg' ),
-  new Recipe('A test Recipe', 'Thi is a simple test', 'http://www.seriouseats.com/recipes/assets_c/2016/01/20160116-american-beef-stew-recipe-34-thumb-1500xauto-429351.jpg' ) ];
+  new Recipe('A test Recipe1', 'This is a simple test', 'http://www.seriouseats.com/recipes/assets_c/2016/01/20160116-american-beef-stew-recipe-34-thumb-1500xauto-429351.jpg' ),
+  new Recipe('A test Recipe2', 'Thi is a simple test', 'http://www.seriouseats.com/recipes/assets_c/2016/01/20160116-american-beef-stew-recipe-34-thumb-1500xauto-429351.jpg' ),
+  new Recipe('A test Recipe3', 'Thi is a simple test', 'http://www.seriouseats.com/recipes/assets_c/2016/01/20160116-american-beef-stew-recipe-34-thumb-1500xauto-429351.jpg' ) ];
   
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+    //console.log(this.recipeWasSelected.emit(recipe))
+  }
 }
